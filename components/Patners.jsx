06ff20b7abd7aa8284163ff8e353685a0842b577
My Name <bukosarkam@gmail.com>
2024-04-06 15:47:01 +0300

    bug fix

--- components/Patners.jsx
+++ components/Patners.jsx
@@ -5,7 +5,7 @@ export default function Patners() {
         <h2 className="text-center text-lg font-semibold leading-8 text-white">
           PARTNERSHIPS AND SUPPORT{" "}
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-2 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           {/* <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="images/maisha.png"
@@ -35,11 +35,11 @@ export default function Patners() {
             height={48}
           />
           <img
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="images/1.png"
             alt="wareflow"
-            width={258}
-            height={88}
+            width={158}
+            height={48}
           />
 
             <img
