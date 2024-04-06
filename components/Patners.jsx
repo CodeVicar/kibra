8a95e17b14141904ef1396a0c9aa212b4f4f432e
My Name <bukosarkam@gmail.com>
2024-04-06 15:54:49 +0300

    maisha

--- components/Patners.jsx
+++ components/Patners.jsx
@@ -5,7 +5,7 @@ export default function Patners() {
         <h2 className="text-center text-lg font-semibold leading-8 text-white">
           PARTNERSHIPS AND SUPPORT{" "}
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-2 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-2 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-6">
           {/* <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="images/maisha.png"
@@ -45,7 +45,14 @@ export default function Patners() {
             <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="images/kenya.png"
-            alt="Kenya"
+            alt="Kenya web design"
+            width={158}
+            height={48}
+            />
+            <img
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+            src="images/maisha.png"
+            alt="Maisha"
             width={158}
             height={48}
             />
